777807b2abb7fe881d7dedd958cec6e029b3e64b
Denis <d.spicyn@gmail.com>
2023-03-21 20:05:18 +0800

    Size base logic created

---
+++ src/views/pages/Size/Components/SizeSubAddEdit.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-row>
+    <v-col>
+      <InlineTextField
+        label="Часть тела*"
+        :value.sync="view.name"
+        :rules="[rules.required]"
+      />
+      <InlineTextField
+        label="Измерение*"
+        :value.sync="view.shortName"
+        :rules="[rules.required]"
+      />
+      <InlineTextField
+        label="Единица измерения"
+        :value.sync="view.shortName"
+        :rules="[rules.required]"
+      />
+    </v-col>
+    <v-col>
+      <InlineTextField
+        label="В разработке, Фото"
+        :value.sync="view.description"
+      />
+    </v-col>
+  </v-row>
+</template>
+<script lang="ts">
+import { Component, Prop, Ref, Watch } from 'vue-property-decorator';
+import InlineTextField from '@/components/InlineTextField.vue';
+import InlineCheckField from '@/components/InlineCheckField.vue';
+import InlineAutocompleteField from '@/components/InlineAutocompleteField.vue';
+import InlineRadioButtonsField from '@/components/InlineRadioButtonsField.vue';
+import InlineSliderField from '@/components/InlineSliderField.vue';
+import Global from '@/mixins/GlobalMixin';
+import ExerciseController, { TExercise, HardSkill } from '@/controllers/ExerciseController';
+import { Mutation, State } from 'vuex-class';
+import Loader from '@/components/Loader.vue';
+
+@Component({
+  components: {
+  InlineTextField,
+  InlineAutocompleteField,
+  InlineCheckField,
+  InlineRadioButtonsField,
+  InlineSliderField,
+  Loader
+  }
+  })
+export default class SizeSubAddEdit extends Global {
+
+}
+
+</script>
---
+++ src/views/pages/Size/SizeAddEdit.vue
@@ -0,0 +1,201 @@
+<template>
+  <section>
+    <v-card>
+      <v-card class="pb-5">
+        <v-card-title v-if="!isEdit" class="darkblue white--text">
+          Добавление замера
+        </v-card-title>
+        <v-card-title v-else class="darkblue white--text">
+          Редактирование замера
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-form
+              ref="form"
+              lazy-validation
+            >
+              <v-row>
+                <v-col>
+                  <InlineTextField
+                    label="Название*"
+                    :value.sync="view.name"
+                    :rules="[rules.required]"
+                  />
+                  <InlineTextField
+                    label="Краткое наименование*"
+                    :value.sync="view.shortName"
+                    :rules="[rules.required]"
+                  />
+                  <inline-radio-buttons-field
+                    label="Базовое упражнение?"
+                    :objects="yesNo"
+                    :selected.sync="view.isBased"
+                  />
+                  <inline-slider-field
+                    label="Сложность"
+                    min="10"
+                    max="30"
+                    step="10"
+                    :hint="hardSkillsHint"
+                    :selected.sync="hardSkills"
+                  />
+                </v-col>
+                <v-col>
+                  <InlineTextField
+                    label="Описание"
+                    :value.sync="view.description"
+                  />
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            class="primary"
+            @click="save"
+          >
+            Сохранить
+          </v-btn>
+          <v-btn
+            class="error"
+            @click="cancel"
+          >
+            Отмена
+          </v-btn>
+          <v-btn
+            class="primary"
+            @click="$router.back()"
+          >
+            Назад
+          </v-btn>
+        </v-card-actions>
+        <Loader :value="isLoading" />
+      </v-card>
+    </v-card>
+  </section>
+</template>
+<script lang="ts">
+import { Component, Prop, Ref, Watch } from 'vue-property-decorator';
+import InlineTextField from '@/components/InlineTextField.vue';
+import InlineCheckField from '@/components/InlineCheckField.vue';
+import InlineAutocompleteField from '@/components/InlineAutocompleteField.vue';
+import InlineRadioButtonsField from '@/components/InlineRadioButtonsField.vue';
+import InlineSliderField from '@/components/InlineSliderField.vue';
+import Global from '@/mixins/GlobalMixin';
+import ExerciseController, { TExercise, HardSkill } from '@/controllers/ExerciseController';
+import { Mutation, State } from 'vuex-class';
+import Loader from '@/components/Loader.vue';
+
+@Component({
+  components: {
+  InlineTextField,
+  InlineAutocompleteField,
+  InlineCheckField,
+  InlineRadioButtonsField,
+  InlineSliderField,
+  Loader
+  }
+  })
+export default class SizeAddEdit extends Global {
+  @Ref('form') readonly form!: any;
+
+  isEdit = false;
+  editId: string | null = null;
+  view: TExercise = new TExercise();
+
+  hardSkills = 10;
+  hardSkillsHint = 'Легко';
+  isLoading = false;
+
+  mounted() {
+    if (this.$route.name === 'ExerciseEdit' && this.$route.params) {
+      this.isEdit = true;
+      this.editId = this.$route.params?.id;
+      this.InitViewEdit();
+    }
+  }
+
+  async InitViewEdit() {
+    try {
+      this.isLoading = true;
+      if (this.editId) {
+        const exercise = await ExerciseController.GetExerciseById(this.editId);
+        this.view = exercise;
+        this.initHardSkills();
+      }
+      else throw new Error('Ошибка, не удалось загрузить упражнение');
+    } catch (error) {
+      if (error instanceof Error) this.showError(error.message);
+    } finally {
+      this.isLoading = false;
+    }
+  }
+
+  initHardSkills() {
+    if (this.view.hardSkill === HardSkill.easy) {
+      this.hardSkills = 10;
+    }
+    else if (this.view.hardSkill === HardSkill.normal) {
+      this.hardSkills = 20;
+    }
+    else if (this.view.hardSkill === HardSkill.hard) {
+      this.hardSkills = 30;
+    }
+  }
+
+  @Watch('hardSkills')
+  selectHardSkills() {
+    if (this.hardSkills === 10) {
+      this.hardSkillsHint = 'Легко';
+      this.view.hardSkill = HardSkill.easy;
+    } else if (this.hardSkills === 20) {
+      this.hardSkillsHint = 'Нормально';
+      this.view.hardSkill = HardSkill.normal;
+    } else if (this.hardSkills === 30) {
+      this.hardSkillsHint = 'Сложно';
+      this.view.hardSkill = HardSkill.hard;
+    }
+  }
+
+  get yesNo() {
+    return [
+      { value: true, text: 'Базовое' },
+      { value: false, text: 'Изолирующее' }
+    ];
+  }
+
+  async save() {
+    if (!this.form.validate()) {
+      this.showInfo('Необходимо заполнить обязательные поля');
+      return;
+    }
+
+    try {
+      this.isLoading = true;
+      if (!this.isEdit) {
+        const exrciseId = await ExerciseController.CreateExercise(this.view);
+        this.showSuccess(`Упражнение ${this.view.name} успешно добавлено с идентификатором ${exrciseId}`);
+      } else if (this.isEdit && this.editId) {
+        const exrciseId = await ExerciseController.UpdateExercise(this.editId, this.view);
+        this.showSuccess(`Упражнение ${this.view.name} с идентификатором ${exrciseId} успешно обновлено!`);
+      }
+      this.goToExercise();
+    } catch (err) {
+      this.showError(`Ошибка. Не удалось добавить/обновить упражнение ${this.view.name}`);
+    } finally {
+      this.isLoading = false;
+    }
+  }
+
+  private goToExercise() {
+    this.$router.push({ name: 'Size' });
+  }
+
+  cancel() {
+    this.goToExercise();
+  }
+}
+
+</script>
